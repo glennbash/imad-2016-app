723aae8acae1cbd829a3cc6bac2535424a32859c
glennbash <lenin.basheer@gmail.com>
2016-10-02 20:05:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
+app.get('/personal', function (req, res) {
+  res.send("Navigated to Personal Page");
+});
+
+app.get('/workbook', function (req, res) {
+  res.send("Navigated to Workbook Page");
+});
+
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
