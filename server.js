bbd38185ac5d318dd026ca1848b7d4548783a604
glennbash <lenin.basheer@gmail.com>
2016-10-09 00:45:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,17 +109,19 @@ app.get('/counter', function(req, res){
     counter=counter+1;
     res.send(counter.toString());
 });
+
+var names=[];
+app.get('/search', function (req, res) {
+    var name=req.query.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
+});
+
 app.get('/:requestName', function (req, res) {
     var requestName=req.params.requestName;
   res.send(createMasterTemplate(contentPages[requestName]));
 });
 
-var names=[];
-app.get('/search/:sstring', function (req, res) {
-    var sstring=req.params.sstring;
-  names.push(sstring);
-  res.send(JSON.stringify(names));
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
